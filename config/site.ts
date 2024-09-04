fe87479a949ea186cc40f52584245eb902c145ed
nainglinnkhant <nainglinnkhnt@gmail.com>
2024-09-04 22:20:56 +0700

    feat(seo): add meta tags and og image

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: "Lingua Time",
+  description:
+    "A smart datetime picker which understands natural language input",
+  url:
+    process.env.NODE_ENV === "production"
+      ? "https://linguatime.nainglinnkhant.com"
+      : "http://localhost:3000",
+}
